9cbabd663f29dc3f84c35c39074a48d5bad75d51
someng <bsm4045@naver.com>
2022-09-28 16:01:29 +0900

    Initial Commit

---
+++ MyAlarmy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyAlarmy
+//
+//  Created by 백소망 on 2022/09/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
